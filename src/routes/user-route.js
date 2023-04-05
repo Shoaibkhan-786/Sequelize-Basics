16bbf052b5aa536307ce6e5677e8a50f2b10a7e0
Shoaib Khan <sk38485868@gmail.com>
2023-04-05 11:32:44 +0530

    initial commit

---
+++ src/routes/user-route.js
@@ -0,0 +1,25 @@
+const { Router } = require('express');
+const { createUser, deleteUser, findAllUser, findById, updateUser, rawQuery, wherePractice, associationCreate, queryInterface } = require('../controllers/user-controllers');
+
+const userRouter = Router({mergeParams:true});
+
+
+userRouter.post('/register', createUser);
+
+userRouter.delete('/delete/:id', deleteUser);
+
+userRouter.get('/', findAllUser);
+
+userRouter.get('/:id', findById);
+
+userRouter.put('/:id', updateUser);
+
+userRouter.get('/raw/query', rawQuery);
+
+userRouter.get('/wherepractice/:id', wherePractice);
+
+userRouter.post('/association-create', associationCreate);
+
+userRouter.get('/query/interface', queryInterface);
+
+module.exports = userRouter;
